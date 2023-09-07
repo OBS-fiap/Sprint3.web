80c151ea86b5930eb9e2f163086a207e18b8e6cd
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-07 18:25:21 -0300

    Criando as rotas das paginas na main

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,54 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Importando as rotas
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Login from './routes/Login/index.jsx'
+import Sobre from './routes/Sobre/index.jsx'
+import Faq from './routes/Faq/index.jsx'
+import EsqueceuSenha from './routes/EsqueceuSenha/index.jsx'
+import Error from './routes/Error/index.jsx'
+
+//Configurar Rotas
+const router = createBrowserRouter ([
+  {
+     path: '/', 
+    element: <App />,
+    errorElement: <Error />,
+    children:[
+      {
+      path: '/',
+      element: <Home />,
+      },
+
+      {
+        path: '/login',
+        element: <Login />,
+      },
+
+      {
+        path: '/sobre',
+        element: <Sobre />,
+      },
+
+      {
+        path: '/faq',
+        element: <Faq />, 
+      },
+
+      {
+        pathq: '/esqueceuSenha',
+        element: <EsqueceuSenha />,
+      },
+    ]
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
