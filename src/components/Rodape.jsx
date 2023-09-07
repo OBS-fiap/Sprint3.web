8fc485bea2c2b0518969b6415c1eeb798322da57
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-07 18:38:17 -0300

    Criando componet Rodape

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Rodape() {
+    
+
+    return(
+        <footer>
+            <a href="#">Facebook</a>
+            <a href="#">E-mail</a>
+            <a href="#">Intagram</a>
+        </footer>
+    )
+}
