ecc02238a9843207b469895048e01f8d5b3b0bba
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-08 01:20:46 -0300

    Rodape componente

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,13 +1,16 @@
 
+import Facebook from '../assets/Facebook.png'
+import Instagram from '../assets/instagram.png'
+import Email from '../assets/email.png'
 
 export default function Rodape() {
-    
-
     return(
         <footer>
-            <a href="#">Facebook</a>
-            <a href="#">E-mail</a>
-            <a href="#">Intagram</a>
+            <div className='social-medias'>
+                <a className='social-media-logo' href="#"> <img className="footer-img" src={Facebook} alt="Logo do facebook"/></a>
+                <a className='social-media-logo' href="#"> <img className="footer-img" src={Email} alt="Logo do e-mail"/></a>
+                <a className='social-media-logo' href="#"> <img className="footer-img" src={Instagram} alt="Logo do Instagram"/></a>
+            </div>
         </footer>
     )
 }
