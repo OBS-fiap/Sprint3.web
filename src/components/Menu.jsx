ea27a16745737c716c68e900e220843377b32483
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-08 01:20:36 -0300

    Criando o menu componente

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 export default function Menu() {
 
     return(
-        <nav className="menu">
-            <Link to="/">Home</Link>
-            <Link to="/sobre">Sobre</Link>
-            <Link to="/faq">Ajuda</Link>
-            <Link to="/login">Login</Link>
-        </nav>
+        <header>
+            <nav className="menu">
+                <Link className="menu-link" to="/">Home</Link>
+                <Link className="menu-link" to="/sobre">Sobre</Link>
+                <Link className="menu-link" to="/faq">Ajuda</Link>
+                <Link className="menu-link" to="/login">Login</Link>
+            </nav>
+        </header>
     )
 }
