4a4bcb56a1290dd7a8d93db8f8b75e6b86f2e43b
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-08 13:32:08 -0300

    Ajustando classes da pagina sobre

--- src/routes/Sobre/index.jsx
+++ src/routes/Sobre/index.jsx
@@ -10,93 +10,103 @@ import Perfil from '../../assets/perfil.png'
 import Dispositivos from '../../assets/dispositivos.png'
 
 export default function Sobre() {
-  return (
-    <div>
-          <main className="principal-sobre">
-            <div className="sobre-conteudo">
-              <h1>Qual o nosso objetivo</h1>
-              <p>is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been
-                the industry's standard dummy text ever since ndustry's standard du ndustry's st
-                the 1500s, when an unknow printer took a galley ndustry's standard du ndustry's s
-                of type and scrambled it to make a type specimen book.</p>
-              </div>
-              <img className="dispositivo-img" src={Dispositivos} alt="Um computador e celular" />
-          </main>
-          <section className="time">
-            <div>
-              <h1>Nosso Time</h1>
-              <p>Conheça a equipe que esta por tras da criação deste projeto</p>
-              <p>Desde a ideação até o desenvolvimento da plataforma</p>
-            </div>
-            <div className="container-integrantes">
-              <div className="integrante">
-                <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
-                <p>Olá, eu sou a <strong>Nicoly</strong></p>
-                <p>RM 552410</p>
-                <p>is simply dummy text of the printing and typesetting industry
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley</p>
-                  <div className="social-media-sobre">
-                    <a href="#"><img src={Github} alt="Logo do Github" /></a>
-                    <a href="#"><img src={Instagram} alt="Logo Instagram" /></a>
-                    <a href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
-                  </div>
-              </div>
-              <div  className="integrante">
-                <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
-                <p>Olá, eu sou a <strong>Nicoly</strong></p>
-                <p>RM 552410</p>
-                <p>is simply dummy text of the printing and typesetting industry
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley</p>
-                    <div className="social-media-sobre">
-                    <a href="#"><img src={Github} alt="Logo do Github" /></a>
-                    <a href="#"><img src={Instagram} alt="Logo Instagram" /></a>
-                    <a href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
-                  </div>
-              </div>
-              <div className="integrante">
-                <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
-                <p>Olá, eu sou a <strong>Nicoly</strong></p>
-                <p>RM 552410</p>
-                <p>is simply dummy text of the printing and typesetting industry
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley</p>
-                    <div className="social-media-sobre">
-                    <a href="#"><img src={Github} alt="Logo do Github" /></a>
-                    <a href="#"><img src={Instagram} alt="Logo Instagram" /></a>
-                    <a href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
-                  </div>
-              </div>
-              <div className="integrante">
-                <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
-                <p>Olá, eu sou a <strong>Nicoly</strong></p>
-                <p>RM 552410</p>
-                <p>is simply dummy text of the printing and typesetting industry
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley</p>
-                  <div className="social-media-sobre">
-                    <a href="#"><img src={Github} alt="Logo do Github" /></a>
-                    <a href="#"><img src={Instagram} alt="Logo Instagram" /></a>
-                    <a href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
-                  </div>
-              </div>
-              <div className="integrante">
-                <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
-                <p>Olá, eu sou a <strong>Nicoly</strong></p>
-                <p>RM 552410</p>
-                <p>is simply dummy text of the printing and typesetting industry
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley</p>
-                  <div className="social-media-sobre">
-                    <a href="#"><img src={Github} alt="Logo do Github" /></a>
-                    <a href="#"><img src={Instagram} alt="Logo Instagram" /></a>
-                    <a href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
-                  </div>
-              </div>
-            </div>
-          </section>
+return (
+<div>
+  <main className="principal-sobre">
+    <div className="sobre-conteudo">
+      <h1 className="sobre-titulo">Qual o nosso objetivo</h1>
+      <p className="sobre-texto">is simply dummy text of the printing and
+        typesetting industry. Lorem Ipsum has been
+        the industry's standard dummy text ever since ndustry's standard du ndustry's st
+        the 1500s, when an unknow printer took a galley ndustry's standard du ndustry's s
+        of type and scrambled it to make a type specimen book.</p>
     </div>
-  );
+    <img className="dispositivo-img" src={Dispositivos} alt="Um computador e celular" />
+  </main>
+  <section className="time">
+    <div className="integrantes-conteudo">
+      <h1 className="sobre-titulo">Nosso Time</h1>
+      <p className="sobre-texto">Conheça a equipe que esta por tras da criação deste projeto</p>
+      <p className="sobre-texto">Desde a ideação até o desenvolvimento da plataforma</p>
+    </div>
+    <div className="container-integrantes">
+      <div className="integrante">
+        <div>
+          <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
+        </div>
+        <p className="integrante-titulo">Olá, eu sou a <strong>Nicoly</strong></p>
+        <p className="integrante-titulo">RM 552410</p>
+        <p className="integrante-texto">is simply dummy text of the printing and typesetting industry
+          Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley</p>
+        <div className="social-media-sobre">
+          <a className="social-media-img" href="#"><img src={Github} alt="Logo do Github" /></a>
+          <a className="social-media-img" href="#"><img src={Instagram} alt="Logo Instagram" /></a>
+          <a className="social-media-img" href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
+        </div>
+      </div>
+      <div className="integrante">
+        <div>
+          <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
+        </div>
+        <p className="integrante-titulo">Olá, eu sou a <strong>Nicoly</strong></p>
+        <p className="integrante-titulo">RM 552410</p>
+        <p className="integrante-texto">is simply dummy text of the printing and typesetting industry
+          Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley</p>
+        <div className="social-media-sobre">
+          <a className="social-media-img" href="#"><img src={Github} alt="Logo do Github" /></a>
+          <a className="social-media-img" href="#"><img src={Instagram} alt="Logo Instagram" /></a>
+          <a className="social-media-img" href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
+        </div>
+      </div>
+      <div className="integrante">
+        <div>
+          <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
+        </div>
+        <p className="integrante-titulo">Olá, eu sou a <strong>Nicoly</strong></p>
+        <p className="integrante-titulo">RM 552410</p>
+        <p className="integrante-texto">is simply dummy text of the printing and typesetting industry
+          Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley</p>
+        <div className="social-media-sobre">
+          <a className="social-media-img" href="#"><img src={Github} alt="Logo do Github" /></a>
+          <a className="social-media-img" href="#"><img src={Instagram} alt="Logo Instagram" /></a>
+          <a className="social-media-img" href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
+        </div>
+      </div>
+      <div className="integrante">
+        <div>
+          <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
+        </div>
+        <p className="integrante-titulo">Olá, eu sou a <strong>Nicoly</strong></p>
+        <p className="integrante-titulo">RM 552410</p>
+        <p className="integrante-texto">is simply dummy text of the printing and typesetting industry
+          Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley</p>
+        <div className="social-media-sobre">
+          <a className="social-media-img" href="#"><img src={Github} alt="Logo do Github" /></a>
+          <a className="social-media-img" href="#"><img src={Instagram} alt="Logo Instagram" /></a>
+          <a className="social-media-img" href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
+        </div>
+      </div>
+      <div className="integrante">
+        <div>
+          <img className="integrante-img" src={Perfil} alt="Foto do integrante" />
+        </div>
+        <p className="integrante-titulo">Olá, eu sou a <strong>Nicoly</strong></p>
+        <p className="integrante-titulo">RM 552410</p>
+        <p className="integrante-texto">is simply dummy text of the printing and typesetting industry
+          Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley</p>
+        <div className="social-media-sobre">
+          <a className="social-media-img" href="#"><img src={Github} alt="Logo do Github" /></a>
+          <a className="social-media-img" href="#"><img src={Instagram} alt="Logo Instagram" /></a>
+          <a className="social-media-img" href=""><img src={Linkedin} alt="Logo do linkedin" /></a>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+);
 }
