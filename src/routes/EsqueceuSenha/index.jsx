abfe389cdaa83bec6ba1053228fbb719a578e40f
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-08 15:49:54 -0300

    Ajustando nome

--- src/routes/EsqueceuSenha/index.jsx
+++ src/routes/EsqueceuSenha/index.jsx
@@ -1,7 +1,18 @@
 export default function EsqueceuSenha() {
     return (
         <div>
-        <h1>Esqueceu Senha</h1>
+        <main className="esqueceu-senha-principal">
+            <div className="esqueceu-senha-container">
+                <img src="" alt="Vetor com uma mulher em duvida da senhna" />
+                <p>Esqueceu sua senha? Sem problemas,</p>
+                <p>Enviaresmos um e-mail para que você possa redefinir</p>
+                <div>
+                    <input type="email" placeholder="Digite o seu e-mail" />
+                </div>
+                <button>Enviar Senha</button>
+            </div>
+
+        </main>
         </div>
     );
     }
