e69ed00f677a22e39677c32227962a5ffc7bfbf8
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-09-12 00:44:02 -0300

    Criando o arquivo index

--- src/routes/AbrirChamadoConfirmacao/index.jsx
+++ src/routes/AbrirChamadoConfirmacao/index.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+import '../../styles/abrirchamado.css'
+
+import SOS from '../../assets/sos.png'
+
 export default function AbrirChamadoConfirmacao() {
     return (
         <div>
-        <h1>AbrirChamadoConfirmacao</h1>
+        <>
+            <main className ="ac-principal">
+                <div className="barra-status"></div>
+                <div className="container-ac-confirmacao">
+                    <img className="ac-img" src={SOS} alt="Moça reportando um ocorrido pelo numero de emergencia" />
+                    <div className="ac-confirmacao-conteudo">
+                        <h1 className="ac-confirmacao-titulo">Recebemos o seu chamado</h1>
+                        <p className="ac-confirmacao-texto">Nossa equipe iniciou o processo para realizar o seu chamado</p>
+                        <p className="ac-confirmacao-texto">Logo menos, nosso prestador chegará até o local</p>
+                        <p className="ac-confirmacao-texto">Você pode acompar todo o processo em <Link to="/meusChamado">MEUS CHAMADOS</Link></p>
+                    </div>
+                </div>
+            </main>
+        </>
         </div>
     );
     }
