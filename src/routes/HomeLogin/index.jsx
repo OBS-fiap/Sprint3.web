f459b0816c501734057ac177b0b6b203160e415a
Vittutexx <vitortex06@gmail.com>
2023-09-12 20:21:12 -0300

    Criando index HomeLogin

--- src/routes/HomeLogin/index.jsx
+++ src/routes/HomeLogin/index.jsx
@@ -1,7 +1,53 @@
-export default function HomeLogin() {
+
+import '../../styles/reset.css';
+import '../../styles/homelogin.css';
+
+    //Importando as fotos
+    
+import Add from '../../assets/adicionar.svg'
+import Pessoa from '../../assets/pessoa.svg';
+import Cancelar from '../../assets/cancelar.svg';
+import Carro from '../../assets/carro.svg';
+import Relogio from '../../assets/relogio.svg';
+
+export default function PaginaInicial() {
     return (
-        <div>
-        <h1>Error</h1>
-        </div>
-    );
-    }
+        <>
+            <main>
+                <div className="main__div__h1">
+                    <h1><b className="main__div__h1__b">OLÁ MARIANA,</b></h1>
+                    <p>Bem vindo(a) de volta!</p>
+                </div>   
+            
+                <navbar className="nav-full-content">
+                    <div className="div_1">
+                        <a href="#">
+                        <span><img src={Pessoa} alt="imagem de uma pessoa"/></span> 
+                            <p>MEU PERFIL</p>
+                        </a>
+                        <a href="#">
+                            <span><img src={Add} alt="imagem de um circulo com mais no meio"/></span>
+                            <p>ABRIR CHAMADO</p>
+                        </a>
+                        
+                        
+                        <a href="#">
+                            <span><img src={Relogio} alt="imagem de um relógio"/></span>
+                            ACOMPANHAR
+                        </a>
+                    </div>
+                    <div className="div_2">
+                        <a href="#">
+                            <span><img src={Carro} alt="formato de um carro"/></span>
+                            ACESSAR VEÍCULOS
+                        </a>
+                        <a href="#">
+                            <span><img src={Cancelar} alt="circulo com x no meio"/></span>
+                            CANCELAR
+                        </a>
+                    </div>
+                </navbar>
+            </main>
+        </>
+    )
+}
