600a35d3fe89de022de21bc25405e9827c8cf18d
Vittutexx <vitortex06@gmail.com>
2023-09-12 20:30:13 -0300

    arrumando erros ortograficos

--- src/routes/MeusChamados/index.jsx
+++ src/routes/MeusChamados/index.jsx
@@ -26,7 +26,7 @@ export default function MeusChamados() {
                         <h4><b>Condutor:</b> Mariana Peixoto </h4>
                     </div>
                     <div className='ocorrido'>
-                    <h4><b>Ocorrido :</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the le.ap into electronic typesetting, remaining essentially unchanged. It was popularised in the 19 60s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h4>
+                    <h4><b>Ocorrido :</b> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the le.ap into electronic typesetting, remaining essentially unchanged. It was popularised in the 19 60s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h4>
                     </div>
 
                     
