f5f9c27c660b36772a3dd2430db2565b33258186
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-12 22:45:23 -0300

    exemplo login

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -6,8 +6,24 @@ import { usuarios } from "../../components/usuarios";
 
 
 export default function Login() {
-    const inputEmail = document.querySelector('#email')
-    const inputPassword = document.querySelector("#password")
+    const inputEmail = document.querySelector('#idEmail')
+    const inputPassword = document.querySelector("#idPassword")
+
+    const navegacao = useNavigate()
+    
+    const Login = () => {
+        const email = inputEmail.value
+        const senha = inputPassword.value
+
+        const usuario = usuarios.find(usuario => usuario.email == email && usuario.senha == senha)
+
+        if(usuario) {
+            return navegacao('/meuPerfil')
+        } else {
+            return navegacao('/login')
+        }
+    }
+    
 
     return (
         <div>
@@ -17,17 +33,17 @@ export default function Login() {
                         <h1 className="login-titulo">Login</h1>
                         <p className="login-texto">Digite os seus dados de acesso no campo abaixo</p>
                     </div>
-                    <form>
+                    <form onSubmit={false}>
                         <div>
                             <label htmlFor="email">E-mail</label>
-                            <input type="email" id="email" placeholder="Digite o seu e-mail"/>
+                            <input type="email" id="idEmail" placeholder="Digite o seu e-mail"/>
                         </div>
                         <div>
                             <label htmlFor="password">Senha</label>
-                            <input type="password" id="password" placeholder="Digite sua senha" />
+                            <input type="password" id="idPassword" placeholder="Digite sua senha" />
                         </div>
                         <Link to="/EsqueceuSenha" className="esqueci-senha">Esqueci minha senha</Link>
-                        <button className="btn-login" type="submit">Acessar</button>
+                        <button className="btn-login" type="submit" onClick={Login}>Acessar</button>
                     </form>
                 </div>
             </main>
